5175af9e45d089cd3f249ec530946e316d2e0a8f
Atul <atulreso1@gmail.com>
2025-03-16 18:46:49 +0530

    BE1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,37 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/user", (req, res) => {
+  res.send("Hello World from server12");
+});
+
+app.get(
+  "/user1",
+  (req, res) => {
+    console.log("handling req 1");
+    res.send("Hello World from server1");
+
+    // if we comment the res.send then it not going to print the second one because first request is not completed so it keep going as request.
+  },
+  (req, res) => {
+    console.log("handling req 2");
+    res.send("Hello World from server2");
+  }
+);
+
+app.get(
+  "/nextUser1",
+  (req, res, next) => {
+    console.log("handling req 1");
+    res.send("Hello World from server1");
+    next();
+    // if we comment the res.send then it  going to print the second one because first request is not completed so node calling the next request.
+  },
+  (req, res) => {j
+    console.log("handling req 2");
+    res.send("Hello World from server2");
+  }
+);
 app.use((req, res) => {
   res.send("Hello World from server");
 });
